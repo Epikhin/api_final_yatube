30a06afbaf775c2bbf7dc9e0fd35f1772f804fb8
Epikhin <114134984+Epikhin@users.noreply.github.com>
2023-01-22 01:39:20 +0700

    6

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -47,7 +47,7 @@ class Follow(models.Model):
         on_delete=models.CASCADE,
         related_name='follower'
     )
-    following = models.ForeignKey( 
+    following = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='following'
